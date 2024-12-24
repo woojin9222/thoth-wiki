a9152af792726aa73ce91082e23c4e1b5ba09d09
잉여개발기 <min08101@naver.com>
2024-12-25 07:53:58 +0900

    버그 수정
    https://github.com/openNAMU/openNAMU/issues/2473

--- views/main_css/js/func/insert_version.js
+++ views/main_css/js/func/insert_version.js
@@ -1,31 +1,31 @@
 "use strict";
 
-function opennamu_do_insert_version(
-    dom_name_version_now, 
-    dom_name_version_new
-) {
-    let url = "/api/version";
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-
-    xhr.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            let get_data = JSON.parse(this.responseText);
-            document.getElementById(dom_name_version_now).innerHTML += get_data['version'];
-            
-            let url_2 = 'https://raw.githubusercontent.com/openNAMU/openNAMU/' + get_data['build'] + '/version.json';
-            let xhr_2 = new XMLHttpRequest();
-            xhr_2.open("GET", url_2);
-            xhr_2.send();
-            
-            xhr_2.onreadystatechange = function() {
-                if(this.readyState === 4 && this.status === 200) {
-                    document.getElementById(dom_name_version_new).innerHTML += JSON.parse(this.responseText)['r_ver'];
-                }
-            }
+function opennamu_do_insert_version(dom_name_version_now, dom_name_version_new) {
+    fetch("/api/version").then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-    }
+        
+        return response.json();
+    }).then(get_data => {
+        document.getElementById(dom_name_version_now).innerHTML += get_data['version'];
+
+        return fetch(`https://raw.githubusercontent.com/openNAMU/openNAMU/${get_data['build']}/version.json`);
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    }).then(versionData => {
+        if(versionData['beta'] !== undefined) {
+            document.getElementById(dom_name_version_new).innerHTML += versionData['beta']['r_ver'];
+        } else {
+            document.getElementById(dom_name_version_new).innerHTML += versionData['r_ver'];
+        }
+    }).catch(error => {
+        console.error('Error:', error);
+    });
+    
 }
 
 let opennamu_do_insert_version_url = [
