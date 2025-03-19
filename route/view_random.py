54cd706900993e19e891bcc991a3431804a56474
잉여개발기 <min08101@naver.com>
2025-03-19 22:21:34 +0900

    버그 수정

--- route/view_random.py
+++ route/view_random.py
@@ -4,6 +4,6 @@ from .go_api_w_random import api_w_random
 
 async def view_random():
     with get_db_connect() as conn:
-        data = await api_w_random()["data"]
+        data = (await api_w_random())["data"]
         
         return redirect(conn, '/w/' + url_pas(data))
