1342d501fa7f787d8477126f2aff374660ce7925
잉여개발기 (SPDV) <min08101@naver.com>
2025-04-26 05:43:14 +0900

    디자인 수정
    https://github.com/openNAMU/openNAMU/issues/2494

--- route/tool/func_render_namumark.py
+++ route/tool/func_render_namumark.py
@@ -1860,7 +1860,7 @@ class class_do_render_namumark:
                 table_data_end = '<table class="' + table_parameter['class'] + '" style="' + table_parameter['table'] + '">' + table_caption + table_data_end + '</table>'
                 table_data_end = '<div class="table_safe" style="' + table_parameter['div'] + '">' + table_data_end + '</div>'
 
-                self.render_data = re.sub(table_regex, lambda x : ('\n<front_br>' + table_data_end + '\n'), self.render_data, 1)
+                self.render_data = re.sub(table_regex, lambda x : ('\n' + table_data_end + '\n'), self.render_data, 1)
 
             table_count_all -= 1
     
